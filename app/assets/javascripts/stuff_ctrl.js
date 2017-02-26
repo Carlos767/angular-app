9b48d3c23432a698d20f3fd60372d8fdfba51ae2
Carlos <school@Carlos-MacBook-Air.local>
2017-02-26 13:13:11 -0800

    Added line validations and manage tasks

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -5,6 +5,22 @@
   angular.module("app").controller("stuffCtrl", function($scope) {
       $scope.message = "Let's find a job!";
       $scope.tasks = ["Find interested jobs", "Submit resumes", "Take a nap"];
+
+      $scope.addTask = function(newTask){
+        if (newTask) {
+          $scope.tasks.push(newTask);
+          $scope.newTask = null;
+          $scope.errorMessage = null;
+        } else {
+          $scope.errorMessage = "Can't be blank";
+        }
+      };
+
+      $scope.completeTask = function(index) {
+        $scope.tasks.splice(index, 1);
+      };
       window.$scope = $scope;
   });
+
+
 }());
